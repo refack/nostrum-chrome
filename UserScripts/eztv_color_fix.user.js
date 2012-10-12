7d4809f05709e9bb22bf52d219ff5be64519d965
Refael Ackermann <refack@gmail.com>
2012-10-12 09:09:34 +0200

    remove 720 from 'uninteresting'

--- UserScripts/eztv_color_fix.user.js
+++ UserScripts/eztv_color_fix.user.js
@@ -8,7 +8,7 @@ GM_addStyle("a.epinfo:visited, a:visited{color: #a0b0c0;}");
 GM_addStyle("a.low_relevance {opacity: 0.5; text-decoration: line-through;}");
 var eps_a = document.getElementsByClassName('epinfo');
 GM_log("eztv eps length = " + eps_a.length)
-var low_relevance_pattern = /720|mvgroup/i
+var low_relevance_pattern = /mvgroup/i
 for (var i in eps_a) {
     var a = eps_a[i];
     if (a.title.search(low_relevance_pattern) != -1)
