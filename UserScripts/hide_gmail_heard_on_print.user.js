1820febf1f3402e7f5da168ccf491dc748b6ab39
Refael Ackermann <refael@empeeric.com>
2013-01-08 08:12:04 +0200

    fix in gmail print

--- UserScripts/hide_gmail_heard_on_print.user.js
+++ UserScripts/hide_gmail_heard_on_print.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name    Hide the Gmail header on print
-// @match   https://mail.google.com/mail/ca/u/0/*view=pt*
+// @match   https://mail.google.com/mail/*view=pt*
 // ==/UserScript==
 
-(function() {
-  var x = document.getElementsByTagName("table")[0];
-  x.parentNode.removeChild(x);
-})()
-  
+var x = document.getElementsByTagName("table")[0];
+x.parentNode.removeChild(x);
+var y = document.getElementsByTagName("hr")[0];
+y.parentNode.removeChild(y); 
