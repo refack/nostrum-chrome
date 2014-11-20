3894e6267e9dab940ce1bd7c543ec6ed380d5dab
Refael Ackermann <refael@empeeric.com>
2014-11-20 13:52:11 +0200

    TBPTables

---
+++ UserScripts/DOI.js
@@ -0,0 +1,7 @@
+javascript:
+(function(o,i,l,e,r){
+e=o.createElement(i);
+r=o.getElementsByTagName(i)[0];
+e.src='//empeeric.herokuapp.com/snippet/cors/22898afdbd4809933985';
+r.parentNode.insertBefore(e,r)
+}(document,'script'));
